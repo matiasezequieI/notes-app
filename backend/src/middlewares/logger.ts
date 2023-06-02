fedad52f33323ad62704d4b30b6b0f501776f991
matiasezequieI <matiasezequiel.qwerty@gmail.com>
2023-06-02 16:27:56 -0300

    feat: improves logger middleware in cases when the access are local

--- backend/src/middlewares/logger.ts
+++ backend/src/middlewares/logger.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
 const logger = (req: Request, res: Response, next: NextFunction) => {
-	console.log(`${req.method}\t${req.headers.origin}\t${req.url}`);
+	console.log(`${req.method}\t${req.headers.origin ? req.headers.origin : 'localhost'}\t${req.url}`);
 	next();
 };
 
