8f4d10141252d534913818d40dd9f61ab4e6253e
matiasezequieI <matiasezequiel.qwerty@gmail.com>
2023-06-05 11:16:24 -0300

    feat: improve logger middleware using morgan package

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
-import logger from './middlewares/logger';
 import endPointNotFound from './middlewares/endpointNotFound';
 import internalServerError from './middlewares/internalServerError';
 import notesRoutes from './routes/notes';
 import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
-app.use(logger);
+app.use(morgan('dev'));
 app.use(cors());
 
 app.use('/api/notes', notesRoutes);
--- backend/src/middlewares/logger.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-
-const logger = (req: Request, res: Response, next: NextFunction) => {
-	console.log(`${req.method}\t${req.headers.origin ? req.headers.origin : 'localhost'}\t${req.url}`);
-	next();
-};
-
-export default logger;
