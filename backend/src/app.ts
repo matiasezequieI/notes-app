96044e40f967bc96dc5119ac47e23a543fa9d611
matiasezequieI <matiasezequiel.qwerty@gmail.com>
2023-06-14 15:49:11 -0300

    removed cors on backed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,14 +3,12 @@ import express from 'express';
 import endPointNotFound from './middlewares/endpointNotFound';
 import errorHandler from './middlewares/errorHandler';
 import notesRoutes from './routes/notes';
-import cors from 'cors';
 import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors());
 
 app.use('/api/notes', notesRoutes);
 
