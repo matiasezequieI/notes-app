f9fc73a8988610c50961e1046a4f57e39eb823a2
matiasezequieI <matiasezequiel.qwerty@gmail.com>
2023-06-15 11:33:13 -0300

    added some style for the notes and added logic for data formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React,{ useEffect, useState } from 'react';
-import './App.css';
-import { Note } from './models/note';
+import { Note as NoteModel } from './models/note';
+import Note from './components/Note';
+import { Col, Container, Row } from 'react-bootstrap';
+import styles from './styles/NotesPage.module.css';
 
 function App() {
-	const [notes, setNotes] = useState<Note[]>([]);
+	const [notes, setNotes] = useState<NoteModel[]>([]);
 
 	useEffect(() => {
 		const loadNotes = async () => { 
@@ -20,11 +22,15 @@ function App() {
 	},[]);
 
 	return (
-		<>
-			<div>
-				{JSON.stringify(notes)}
-			</div>
-		</>
+		<Container>
+			<Row xs={1} md={2} xl={3} className='g-4'>
+				{notes.map(note => (
+					<Col key={note._id}>
+						<Note note={note} className={styles.note} />
+					</Col>
+				))}
+			</Row>
+		</Container>
 	);
 }
 
