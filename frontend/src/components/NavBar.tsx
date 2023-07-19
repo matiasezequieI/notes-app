0f2599b87b55a0e7ccef029a625e3ffde3285b33
matiasezequieI <matiasezequiel.qwerty@gmail.com>
2023-07-19 16:24:45 -0300

    implemented link to privacy page and homepage in the navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,16 +3,22 @@ import NavBarProps from '../interfaces/NavBarProps';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import NavBarLoggedInView from './NavBarLoggedInView';
 import NavBarLoggedOutView from './NavBarLoggedOutView';
+import { Link } from 'react-router-dom';
 
 const NavBar = ({loggedInUser, onSignUpClicked, onLoginClicked, onLogoutSuccessful}: NavBarProps) => {
 	return ( 
 		<Navbar bg="primary" variant="dark" expand="sm" sticky="top">
 			<Container>
-				<Navbar.Brand>
+				<Navbar.Brand as={Link} to='/'>
 									Cool Notes App
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="main-navbar" />
 				<Navbar.Collapse id="main-navbar">
+					<Nav>
+						<Nav.Link as={Link} to='/privacy'>
+							Privacy
+						</Nav.Link>
+					</Nav>
 					<Nav className="ms-auto">
 						{ loggedInUser
 							? <NavBarLoggedInView user={loggedInUser} onLogoutSuccessful={onLogoutSuccessful} />
